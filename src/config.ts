418a6e5698b80a43d387f2925d22c4cf05cb3a7f
deep73599 <deep4357900@gmail.com>
2026-05-02 12:21:37 +0530

    Change SITE_TITLE and SITE_DESCRIPTION values
    Updated site title and description to reflect new information.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Singh Deepak Randhee';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Singh Deepak Randheer';
+export const SITE_DESCRIPTION = 'Android Application Engineer specializing in Kotlin, Firebase, and Jetpack Compose. Junior Android Developer with experience in corporate software environments and an RYA-certified Coastal Skipper working toward global circumnavigation.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
